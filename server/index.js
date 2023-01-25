2f6ad3c8c65d845b70698647c1ed958fc7c7931c
Marvellous Makuyana <marvellousmakuyana@gmail.com>
2023-01-25 14:28:08 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoute from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoute);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Test:mufaro@cluster0.rmog1sv.mongodb.net/?retryWrites=true&w=majority";
+const PORT  = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    //.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+   // .catch((error) => console.log(error.message));
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+//mongoose.set("useFindAndModify", false); 
