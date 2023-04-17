74fc0997113b4b86407f4f0823a978c82fa2cd85
Marvellous Makuyana <marvellousmakuyana@gmail.com>
2023-04-17 13:24:54 +0200

    created first post and save to DB

--- server/index.js
+++ server/index.js
@@ -7,17 +7,21 @@ import postRoute from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoute);
-
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://Test:mufaro@cluster0.rmog1sv.mongodb.net/?retryWrites=true&w=majority";
-const PORT  = process.env.PORT || 5000;
+app.use("/posts", postRoute);
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-    //.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-   // .catch((error) => console.log(error.message));
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-//mongoose.set("useFindAndModify", false); 
+const CONNECTION_URL =
+  "mongodb+srv://Test:mufaro@cluster0.rmog1sv.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+// .catch((error) => console.log(error.message));
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+//mongoose.set("useFindAndModify", false);
